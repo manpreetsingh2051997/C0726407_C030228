2a8eb7d846aa529b3c25a9d9372b3dfae495c8a9
visualstudio <visualstudio@122-PC07>
2019-03-15 18:06:35 -0400

    FinalCommit for first code

--- C0726407_C030228/Program.cs
+++ C0726407_C030228/Program.cs
@@ -52,4 +52,38 @@ namespace BankAccountNS
             m_balance += amount; // intentionally incorrect code
         }
 
-      
+        public void Credit(double amount)
+        {
+            if (m_frozen)
+            {
+                throw new Exception("Account frozen");
+            }
+
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            m_balance += amount;
+        }
+
+        private void FreezeAccount()
+        {
+            m_frozen = true;
+        }
+
+        private void UnfreezeAccount()
+        {
+            m_frozen = false;
+        }
+
+        public static void Main()
+        {
+            BankAccount ba = new BankAccount("Mr. Bryan Walton", 11.99);
+
+            ba.Credit(5.77);
+            ba.Debit(11.22);
+            Console.WriteLine("Current balance is ${0}", ba.Balance);
+        }
+    }
+}
